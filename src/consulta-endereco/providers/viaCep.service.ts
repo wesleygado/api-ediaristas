2039ee7ebc7b609af796df76bdb820741f7cd687
Wesley Gado <wesleygado@gmail.com>
2022-03-10 22:32:26 -0300

    tratamento exceptions viaCep

--- src/consulta-endereco/providers/viaCep.service.ts
+++ src/consulta-endereco/providers/viaCep.service.ts
@@ -1,14 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { EnderecoResponse } from '../dtos/enderecoResponse.dto';
 import axios from 'axios';
 
 @Injectable()
 export class ViaCepService {
+  DIARISTAS = new EnderecoResponse();
   async buscarEnderecoPorCep(cep: string): Promise<EnderecoResponse> {
     const URL_CEP = `http://viacep.com.br/ws/${cep}/json/`;
 
-    const enderecoBody = await axios.get(URL_CEP);
-    const response: EnderecoResponse = enderecoBody.data;
-    return response;
+    try {
+      const enderecoBody = await axios.get(URL_CEP);
+      this.DIARISTAS = enderecoBody.data;
+    } catch (error) {
+      if (error.response['status'] === 400) {
+        throw new BadRequestException('CEP INVÁLIDO');
+      }
+
+      if (error.response['status'] === 500) {
+        throw new InternalServerErrorException('ERRO INTERNO');
+      }
+    }
+
+    if (this.DIARISTAS['erro'] === true) {
+      throw new NotFoundException('CEP NÃO ENCOTRADO');
+    }
+
+    return this.DIARISTAS;
   }
 }
--- src/diaristas/diarista.controller.ts
+++ src/diaristas/diarista.controller.ts
@@ -1,17 +1,9 @@
-import { HttpService } from '@nestjs/axios';
 import { Controller, Get, Param } from '@nestjs/common';
-import { ViaCepService } from 'src/consulta-endereco/providers/viaCep.service';
-import { DiaristaMapper } from './diarista.mapper';
 import { DiaristaService } from './diarista.service';
 
 @Controller('api/diaristas')
 export class DiaristaController {
-  constructor(
-    private readonly diaristaService: DiaristaService,
-    private httpService: HttpService,
-    private viaCepService: ViaCepService,
-    private diaristaMapper: DiaristaMapper,
-  ) {}
+  constructor(private readonly diaristaService: DiaristaService) {}
 
   @Get('/localidades/:cep')
   async findByCep(@Param('cep') cep: string) {
